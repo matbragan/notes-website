6f433d4d0ac83656df65512b6758afa5589b9117
matbragan <matheus.bs.oliveira@gmail.com>
2023-06-09 19:07:47 -0300

    update note routes

--- routes/notes.js
+++ routes/notes.js
@@ -4,14 +4,34 @@ const {ObjectId} = require('mongodb');
 
 const router = Router();
 
-// creation route form
+// note creation route
 router.get('/', function(red, res) {
 
     res.render('notes/create');
 
 })
 
-// post note for database insertion
+// note detail route
+router.get('/:id', async function(req, res) {
+
+    const id = new ObjectId(req.params.id);
+    const note = await db.getDb().db().collection('notes').findOne({ _id: id });
+
+    res.render('notes/detail', {note});
+
+})
+
+// note edition route
+router.get('/edit/:id', async function(req, res) {
+
+    const id = new ObjectId(req.params.id);
+    const note = await db.getDb().db().collection('notes').findOne({ _id: id });
+
+    res.render('notes/edit', {note});
+
+})
+
+// post note - database insert
 router.post('/', function(req, res) {
 
     const data = req.body;
@@ -22,6 +42,38 @@ router.post('/', function(req, res) {
         .db()
         .collection('notes')
         .insertOne({ title: title, description: description });
+
+    res.redirect(301, '/');
+
+})
+
+// post note - database delete
+router.post('/delete', function(req, res) {
+
+    const data = req.body;
+    const id = new ObjectId(data.id);
+
+    db.getDb()
+        .db()
+        .collection('notes')
+        .deleteOne({ _id: id });
+
+    res.redirect(301, '/');
+
+})
+
+// post note - database update
+router.post('/update', function(req, res) {
+
+    const data = req.body;
+    const id = new ObjectId(data.id);
+    const title = data.title;
+    const description = data.description;
+
+    db.getDb()
+        .db()
+        .collection('notes')
+        .updateOne({_id: id}, {$set: {title: title, description: description}});
 
     res.redirect(301, '/');
 
